ced4edd296906d766d9676f631261cab1b87a865
Hxzithx <hasithalakruwan081@gmail.com>
2026-01-18 11:44:04 +0530

    second commit

---
+++ index.ts
@@ -0,0 +1,18 @@
+function openForm() : void {
+    const popup = document.getElementById("popupForm") as HTMLDivElement;
+    popup.style.display = "flex";
+}
+
+function closeForm() : void {
+    const closeForm = document.getElementById("popupForm") as HTMLDivElement;
+    closeForm.style.display = "none";
+}
+function saveTask() : void {
+    const task = document.getElementById("taskInput") as HTMLInputElement;
+
+    if(task.value.trim() !== ""){
+         alert("Task Added" +task.value);
+         task.value = "";
+         closeForm();
+    }
+}
