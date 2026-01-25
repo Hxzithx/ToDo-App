9d594dfe041113b2522e0afad8513c2bce779b46
Hxzithx <hasithalakruwan081@gmail.com>
2026-01-25 15:16:48 +0530

    update controller

--- controller/taskController.ts
+++ controller/taskController.ts
@@ -1,3 +1,5 @@
+import { Request, Response } from "express";
+
 import{
     getAllTasksfromDB,
     addTasktoDB,
@@ -7,10 +9,25 @@ import{
 
 export const getAllTask = async (req : Request, res : Response) => {
     try{
-       const tasks = await getAllTasksfromDB;
+       const tasks = await getAllTasksfromDB();
        res.status(200).json(tasks);
     }
     catch(error){
        res.status(500).json({message : "Faild to fetch task"});
+    }
+}
+
+export const addTask = async (req : Request,res : Response ) => {
+    try{
+    const { title } = req.body;
+
+     if(!title || typeof title!== "string"){
+        return res.status(400).json({message : "Task Name is Required"});
+     }
+     const newTask = await addTasktoDB(title);
+     res.status(201).json(newTask);
+    }
+    catch(error){
+        res.status(500).json({message : "Failed to Create Task"})
     }
 }
