3e5eb4415c88411fd9137784cf4a7e50667c803b
Hxzithx <hasithalakruwan081@gmail.com>
2026-01-28 19:36:42 +0530

    user auth (not completed)

--- FE/index.js
+++ FE/index.js
@@ -98,3 +98,74 @@ app.use("/api", routes_1.default);
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  setCurrentDate();
+});
+
+function setCurrentDate() {
+  const dateElement = document.getElementById("currentDate");
+
+  if (!dateElement) {
+    console.error("Date element not found");
+    return;
+  }
+
+  const today = new Date();
+
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, "0");
+  const day = String(today.getDate()).padStart(2, "0");
+
+  dateElement.textContent = `${year}/${month}/${day}`;
+}
+function showAppPage() {
+  document.getElementById("authPage").style.display = "none";
+  document.getElementById("appPage").style.display = "block";
+}
+
+function showAuthPage() {
+  document.getElementById("authPage").style.display = "flex";
+  document.getElementById("appPage").style.display = "none";
+}
+
+// Firebase auth functions
+window.signup = async function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await createUserWithEmailAndPassword(auth, email, password);
+    alert("Signup successful!");
+    showAppPage();
+  } catch (error) {
+    alert(error.message);
+  }
+};
+
+window.login = async function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    alert("Login successful!");
+    showAppPage();
+  } catch (error) {
+    alert(error.message);
+  }
+};
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAIK0D3hkDX-OPlS1xnz6nMRJc59NB9Tco",
+  authDomain: "todo-app-74c70.firebaseapp.com",
+  projectId: "todo-app-74c70",
+  storageBucket: "todo-app-74c70.firebasestorage.app",
+  messagingSenderId: "96127534578",
+  appId: "1:96127534578:web:a827d3959129058cb8176b",
+  measurementId: "G-82KSC33CS8"
+};
+const { initializeApp, getAuth } = window.firebaseModules;
+
+const fireBaseapp = initializeApp(firebaseConfig);
+const auth = getAuth(fireBaseapp);
